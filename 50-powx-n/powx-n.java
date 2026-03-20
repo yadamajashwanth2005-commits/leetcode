24f9cfb009a8441061856cd9ce1e444701f9e26d
yadamajashwanth2005-commits <yadamajashwanth2005@gmail.com>
2026-03-20 20:24:30 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (44.28%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;  
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double result = 1;
+        while (N > 0) {
+            if ((N & 1) == 1) { 
+                result = result * x;
+            }
+            x = x * x;
+            N = N / 2;
+        }
+        return result;
+    }
+}
